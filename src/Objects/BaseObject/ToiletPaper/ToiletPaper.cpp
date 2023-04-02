c988777c1866ca7a3d8b1842d9d5eb110c2f0a58
Wisnu Y <wy.stsmail@gmail.com>
2023-04-02 22:02:04 +0700

    Toilet Paper Base Object
    file cpp & hpp Toilet Paper

---
+++ src/Objects/BaseObject/ToiletPaper/ToiletPaper.cpp
@@ -0,0 +1,14 @@
+#include "ToiletPaper.hpp"
+
+using namespace std;
+private int Position;
+
+void ToiletPaper::getPosition()
+{
+    cout << "Position" << endl;
+}
+
+void ToiletPaper::setPositon()
+{
+    Position = Position;
+}
---
+++ src/Objects/BaseObject/ToiletPaper/ToiletPaper.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+
+class ToiletPaper : public BaseObject
+{
+public:
+    void getPosition() override;
+    void setPosition() override;
+};
